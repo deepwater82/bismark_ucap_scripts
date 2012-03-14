6308a5107f7a57a2dcf638248b9ac2fa3c29083a
Hyojoon <deepwater82@gmail.com>
2012-03-14 00:03:54 -0400

    update enable_ucap

--- enable_ucap.sh
+++ enable_ucap.sh
@@ -6,7 +6,7 @@ uci set wireless.@wifi-iface[1].network=''
 uci set network.lan.ifname=''
 # OpenFlow
 uci set openflow.@ofswitch[0].dp=dp0
-local dp_id=`ifconfig | grep "eth0 " | grep HWaddr | awk -F 'HWaddr ' '{ print $2 }' | sed -e '/:/ s/://g'}`
+local dp_id=`ifconfig | grep "^eth0 " | grep HWaddr | awk -F 'HWaddr ' '{ print $2 }' | sed -e '/:/ s/://g'}`
 uci set openflow.@ofswitch[0].dpid=$dp_id
 uci set openflow.@ofswitch[0].ofports=wlan0,wlan1,eth0.1,veth0
 uci set openflow.@ofswitch[0].ofctl=tcp:143.215.131.178:6633
